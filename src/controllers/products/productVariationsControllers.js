41ecba79722812b9ee734e201325bb11a3197aa0
Aimeana100 <aimeanathole@gmail.com>
2023-03-27 16:30:32 +0200

    feat(vendor-update-product) This changes added will enable an authanticated user as a vendor / seller to be able to updated the products from his own collection
    - add products , products attributes  and product Images modification.
    - add swagger documentation for the routes
    - restructured the doc responses to remove code duplicates
    
    Contributor @BenoitIT

--- src/controllers/products/productVariationsControllers.js
+++ src/controllers/products/productVariationsControllers.js
@@ -2,11 +2,13 @@ import db from '../.././models';
 import { checkEmptyFields } from '../../utils/validations/handlingEmptyFields';
 import { asyncWrapper } from '../../utils/handlingTryCatchBlocks';
 import { grabbingImage } from '../../utils/grabbingImages';
+import { removeImageFromCloudinary } from '../../utils/handlingFileUploads';
+
 //defining execution of images uploads
 export const addNewProductVariation = asyncWrapper(async (req, res) => {
 
   // Get the data for the new product from the request body
-  const { price, sizeId, color, productId,quantity } = req.body;
+  const { price, size, color, productId,quantity } = req.body;
   // Check if the required fields are provided
   const areAllNotFilled = checkEmptyFields(req, res);
   if (!areAllNotFilled) {
@@ -37,3 +39,21 @@ export const addNewProductVariation = asyncWrapper(async (req, res) => {
     });
   }
 });
+
+export const updateProductVariaton = asyncWrapper( async (req, res) => {
+  const {id} = req.params
+  if(!id) return res.status({ststus: req.t('fail'), message: 'Id ' + req.t('is_required') })
+
+  const row  = await db.ProductAttribute.findByPk(id)
+  if(!row) return res.status(404).json({ststus: req.t('fail'), message: req.t('no_product_variation_found')})
+
+  const urls = await grabbingImage(req);
+  if(urls.length > 0){
+    req.body['attrImage'] = urls[0].url;
+    removeImageFromCloudinary(row.cloudinaryId);
+  }
+
+  row.set(req.body);
+  await row.save();
+  return res.status(200).json({ststus: req.t('success'), message: req.t('product_variation_updated')})
+})
