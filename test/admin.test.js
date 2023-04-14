47d64e67bb9be288ceb525b1d1972079c5a50a4b
benoitIT <benoitnda35@gmail.com>
2023-04-14 17:20:44 +0200

    adding shopping cart tests

--- test/admin.test.js
+++ test/admin.test.js
@@ -115,6 +115,102 @@ describe('ADMINS', () => {
     const res = await chai.request(app).get(`/api/admin/users/${adminId}`);
     res.statusCode.should.equal(403);
   });
+  
+  it('disable an eisting user', async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users/${adminId}/disable`)
+      .set('Authorization', `bearer ${token}`);
+    res.statusCode.should.equal(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('message');
+    res.body.should.have.property('status');
+  })
+
+  it('disable a non existing user', async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users/714bf693-a1c4-4f04-98f1-1323289f4008/disable`)
+      .set('Authorization', `bearer ${token}`);
+    res.statusCode.should.equal(404);
+    res.body.should.be.a('object');
+    res.body.should.have.property('message');
+    res.body.should.have.property('status');
+  })
+
+  it('should return forbidden error status code 403', async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users/${adminId}/disable`)
+      .set('Authorization', `bearer ${token}`);
+    res.statusCode.should.equal(403);
+  })
+
+  it('enable a non existing user', async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users/714bf693-a1c4-4f04-98f1-1323289f4008/enable`)
+      .set('Authorization', `bearer ${token}`);
+    res.statusCode.should.equal(404);
+    res.body.should.be.a('object');
+    res.body.should.have.property('message');
+    res.body.should.have.property('status');
+  })
+
+  it('should enable an existing user', async() => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users/${adminId}/enable`)
+      .set('Authorization', `bearer ${token}`);
+    res.statusCode.should.equal(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('message');
+    res.body.should.have.property('status');
+  })
+
+  it('should return forbidden error status code 403', async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users/${adminId}/enable`)
+      .set('Authorization', `bearer ${token}`);
+    res.statusCode.should.equal(403);
+  })
+ 
+  it(' should update user profile', async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users`)
+      .set('Authorization', `bearer ${token}`)
+      .send({
+        firstname: 'newfirstname',
+        lastname: 'newlastname',
+        email: 'john.doe@example.com',
+        phone: '123-456-7890'        
+  })
+  
+    res.statusCode.should.equal(200);
+    res.body.should.be.a('object');
+    res.body.should.have.property('message');
+    res.body.should.have.property('status');
+  })
+
+  it('should return a 404 status code if the user is not found when updating', async () => {
+    const res = await chai
+      .request(app)
+      .put(`/api/v1/users`)
+      .set('Authorization', `bearer e${token}`)
+      .send({
+        firstname: 'newfirstname',
+        lastname: 'newlastname',
+        email: 'john.doe@example.com',
+        phone: '123-456-7890'        
+  })
+  res.statusCode.should.equal(401);
+  res.body.should.be.a('object');
+  res.body.should.have.property('message');
+  res.body.should.have.property('status');
+  })
+
 
   it('deleting a single user with Authorization. it should return 200', async () => {
     const res = await chai
