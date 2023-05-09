489c9067404cc038fe57633fc55c9b144084c03d
Fabrice <mwanafunzifabrice@gmail.com>
2023-05-10 01:25:35 +0200

    issue[ add more info on wishlist's response ]
    - add the name of product that being added to wishlist
    
    issue [finsh add more info on wishlist's response]

--- src/controllers/roles/getRoleController.js
+++ src/controllers/roles/getRoleController.js
@@ -11,18 +11,19 @@ export const getRole = asyncWrapper(async (req, res) => {
 });
 
 export const updateUserRole = asyncWrapper(async (req, res) => {
-    const { id, role } = req.body;
-    const user = await db.user.findByPk(id);
-    if (!user) {
-        return res.status(404).json({
-            status: req.t('fail'),
-            message: req.t('not_found'),
-        });
-    }
-    user.roleId = role;
-    await user.save();
-    res.status(200).json({
-        status: req.t('success'),
-        message: req.t('role_updated'),
+  const { id, role } = req.body;
+  const user = await db.user.findByPk(id);
+  if (!user) {
+    return res.status(404).json({
+      status: req.t('fail'),
+      message: req.t('not_found'),
     });
+  }
+  user.roleId = role;
+  await user.save();
+  res.status(200).json({
+    status: req.t('success'),
+    message: req.t('role_updated'),
+    data: user,
+  });
 });
