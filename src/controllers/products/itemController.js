5220c12f195dbb2a2bea86fbac0183ea7b6efc86
Fabrice <mwanafunzifabrice@gmail.com>
2023-05-19 11:45:10 +0200

    bug[fix search] - return searched product and its variation - remove pagination bug[fix search]

--- src/controllers/products/itemController.js
+++ src/controllers/products/itemController.js
@@ -5,10 +5,6 @@ import { asyncWrapper } from "../../utils/handlingTryCatchBlocks";
 // Get all Product of a seller
 export const getAllSellerItems = asyncWrapper(async (req, res) => {
 
-    // Set up pagination options
-    const page = req.query.page || 1;
-    const limit = req.query.limit || 10;
-    const offset = (page - 1) * limit;
     // Find all products registered by the user with pagination
     const products = await db.Product.findAndCountAll({
         where: {
@@ -26,21 +22,12 @@ export const getAllSellerItems = asyncWrapper(async (req, res) => {
             },
             
           ],
-        limit,
-        offset,
     });
-    // Return the paginated results
-    const totalPages = Math.ceil(products.count / limit);
-    const currentPage = parseInt(page);
-    const itemsPerPage = parseInt(limit);
+    
     if (products) {
         res.status(200).json({
             status: req.t('success'),
             data: {
-                totalItems: products.count,
-                totalPages,
-                currentPage,
-                itemsPerPage,
                 products: products.rows,
             },
             message:req.t('productRetieved'),
@@ -48,10 +35,6 @@ export const getAllSellerItems = asyncWrapper(async (req, res) => {
     } else {
         res.status(404).json({
             data: {
-                totalItems: products.count,
-                totalPages,
-                currentPage,
-                itemsPerPage,
                 products: products.rows,
             },
             message:req.t('product_not_found')
@@ -60,12 +43,6 @@ export const getAllSellerItems = asyncWrapper(async (req, res) => {
 })
     //Get all available Products for sale
     export const getAllBuyerItems = asyncWrapper(async (req, res) => {
-
-        // Set up pagination options
-        const page = req.query.page || 1;
-        const limit = req.query.limit || 10;
-        const offset = (page - 1) * limit;
-        // Find all products registered by the user with pagination
         const products = await db.Product.findAndCountAll({
           where: {
             status :'AVAILABLE'
@@ -81,21 +58,11 @@ export const getAllSellerItems = asyncWrapper(async (req, res) => {
             },
             
           ],
-            limit,
-            offset,
-         
         });
-        // Return the paginated results
-        const totalPages = Math.ceil(products.count / limit);
-        const currentPage = parseInt(page);
-        const itemsPerPage = parseInt(limit);
+
         res.status(200).json({
             status: req.t('success'),
             data: {
-                totalItems: products.count,
-                totalPages,
-                currentPage,
-                itemsPerPage,
                 products: products.rows,
             },
             message: req.t('productRetieved'),
